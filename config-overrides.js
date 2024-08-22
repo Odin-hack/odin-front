982f8b24ea8460f931d625f30e17f7831b6d3b05
fewcatltd <137312580+fewcatltd@users.noreply.github.com>
2024-08-23 04:37:25 +0500

    rewrite all private vars

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const { override, addBabelPlugin, babelInclude } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addBabelPlugin([
+    '@babel/plugin-proposal-private-methods',
+    { loose: true }
+  ]),
+  addBabelPlugin([
+    '@babel/plugin-proposal-class-properties',
+    { loose: true }
+  ]),
+  addBabelPlugin([
+    '@babel/plugin-proposal-private-property-in-object',
+    { loose: true }
+  ]),
+  babelInclude([
+    path.resolve('src'), // Включаем транспиляцию для src
+    path.resolve('node_modules') // Включаем транспиляцию для всех модулей
+  ])
+);
