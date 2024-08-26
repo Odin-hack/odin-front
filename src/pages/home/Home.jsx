bff84ddaf9d3559ce009641246a409fbdc654d91
serdnaley <serdnaley@gmail.com>
2024-08-26 21:07:31 +0200

    Temp alert

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -824,6 +824,7 @@ export const Home = () => {
     tonConnectUI.onStatusChange(wallet => {
       if (wallet.connectItems?.tonProof && 'proof' in wallet.connectItems.tonProof) {
         try {
+          alert('GOTCHA!!!')
           dispatch(slices.homePageSlice.thunks.registerWallet({address: wallet.account.address}))
           components.toast.showText('TON Wallet connected')
         } catch (err) {
