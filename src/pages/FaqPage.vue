261fde6c680f8d6dbaefeaba074471065f7b429d
fewcatltd <dokotov@gmail.com>
2024-11-21 12:05:27 +0400

    faq

--- src/pages/FaqPage.vue
+++ src/pages/FaqPage.vue
@@ -6,27 +6,36 @@ const contents = [
   },
   {
     title: 'Your Goal is Simple',
-    text: 'Your task is to find a valid hash for the current block during mining and earn rewards for it.\n70% of the block reward goes to the miner who discovers the correct hash, while 30% is shared among other participants.\n\nNote: The miner reward gradually decreases by 10% every 100,000 blocks until it reaches a fixed 10%.',
+    text: 'Your task is to find a valid hash for the current block during mining and earn rewards for it.\n' +
+        '60% of the block reward goes to the miner who discovers the correct hash, while 40% is shared among other participants.\n\n' +
+        'Note: The miner reward gradually decreases by 10% every 100,000 blocks until it reaches a fixed 10%.',
   },
   {
     title: 'Mining Mechanism',
-    text: 'Imagine mining cryptocurrency directly from your phone. What would Satoshi think if he saw our project?!\n\nMining operates on the same principles as Bitcoin, using a similar algorithm.\nHowever, our mechanism is accessible through Telegram MiniApps, allowing you to mine both on mobile devices and desktop computers.',
+    text: 'Imagine mining cryptocurrency directly from your phone. What would Satoshi think if he saw our project?!\n\n' +
+        'Mining operates on the same principles as Bitcoin, using a similar algorithm.\n' +
+        'However, our mechanism is accessible through Telegram MiniApps, allowing you to mine both on mobile devices and desktop computers.',
   },
   {
     title: 'Early Miner Rewards',
-    text: 'The more people start mining, the harder it becomes to earn tokens, and vice versa.\nBeing an early adopter is always more beneficial: you have a greater chance of earning maximum rewards for solved blocks while the competition is still low.',
+    text: 'The more people start mining, the harder it becomes to earn tokens, and vice versa.\n' +
+        'Being an early adopter is always more beneficial: you have a greater chance of earning maximum rewards for solved blocks while the competition is still low.',
   },
   {
     title: 'Limited Mining',
-    text: 'Once all tokens are mined, they will be distributed on-chain among the miners.\nThe total mining period lasts approximately 2 to 3 months, followed by an AirDrop and token listing on both DEX and CEX platforms.',
+    text: 'Once all tokens are mined, they will be distributed on-chain among the miners.\n' +
+        'The total mining period lasts approximately 2 to 3 months, followed by an AirDrop and token listing on both DEX and CEX platforms.',
   },
   {
     title: 'Token Distribution',
-    text: 'Our project features a unique mining mechanism that works seamlessly on mobile devices and desktops. This innovative approach targets users who appreciate the mining process itself.\n\nMining is just the first step in our technological roadmap. We are committed to rewarding all users who actively mine blocks and contribute to the platform\'s development. Up to 20% of SIGMA’s revenue earned through Telegram Stars will be allocated to further development and marketing, while the rest will directly support the token, user rewards, and its listing. The crypto community will be the driving force in the new bull cycle!',
+    text: 'Our project features a unique mining mechanism that works seamlessly on mobile devices and desktops. This innovative approach targets users who appreciate the mining process itself.\n' +
+        '\n' +
+        'Mining is just the first step in our technological roadmap. We are committed to rewarding all users who actively mine blocks and contribute to the platform\'s development. Up to 20% of SIGMA’s revenue earned through Telegram Stars will be allocated to further development and marketing, while the rest will directly support the token, user rewards, and its listing. The crypto community will be the driving force in the new bull cycle!',
   },
   {
     title: 'Device Performance',
-    text: 'Since your device performs real computations, it may heat up only in maximum Nitro mode.\nFor the rest of the time, you can mine worry-free without overheating concerns.',
+    text: 'Since your device performs real computations, it may heat up only in maximum Nitro mode.\n' +
+        'For the rest of the time, you can mine worry-free without overheating concerns.',
   },
   {
     title: 'Limitations',
